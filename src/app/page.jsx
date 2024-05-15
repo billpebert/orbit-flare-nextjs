a7fd28bae96ae0d8a3a39d6ddc7ecb08fbe584ad
billpebert <bpgponto@gmail.com>
2024-05-15 12:23:08 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="px-6 py-9 size-56 rounded-lg bg-gradient-to-br from-[rgba(255,255,255,0.5)] to-[rgba(255,255,255,0)]"></div>
+    </div>
+  )
+};
+
+export default Home;
